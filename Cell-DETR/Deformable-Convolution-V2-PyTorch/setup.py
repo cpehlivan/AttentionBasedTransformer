b70086644bc0ba36035ab5218dd763a67a9edb28
cpehlivan <can.pehlivan1998@hotmail.de>
2023-09-17 15:56:57 +0200

    changed CUDA_HOME to /opt/cuda

--- Cell-DETR/Deformable-Convolution-V2-PyTorch/setup.py
+++ Cell-DETR/Deformable-Convolution-V2-PyTorch/setup.py
@@ -27,10 +27,11 @@ def get_extensions():
     extra_compile_args = {"cxx": []}
     define_macros = []
 
-    CUDA_HOME = "/opt/cuda"
+
 
 
     if torch.cuda.is_available() and CUDA_HOME is not None:
+        CUDA_HOME = "/opt/cuda"
         extension = CUDAExtension
         sources += source_cuda
         define_macros += [("WITH_CUDA", None)]
